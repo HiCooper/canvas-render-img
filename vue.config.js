d9bc27fce310f41fe0ec7599f79b8dc2050814f8
caoxuean <Kakaxi123>
2021-05-19 09:48:35 +0800

    'x'

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   lintOnSave: false,
   assetsDir: 'static',
+  publicPath: 'canvas-render-img',
   configureWebpack: config => {
     if (process.env.NODE_ENV === 'production') {
       // 为生产环境修改配置...
